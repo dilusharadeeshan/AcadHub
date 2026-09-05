20845cbaf76152d9f2eeef621507d6b7269d2218
dilusharadeeshan <dilusharadeeshan@gmail.com>
2026-09-05 08:34:08 +0530

    feat: add protected routes for student profile retrieval and logout functionality

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,11 +1,17 @@
 import express from "express";
 import { registerStudent,
-        loginStudent 
+        loginStudent,
+        getProfile,
+        logoutStudent 
  } from "../controllers/authController.js";
+
+import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/register", registerStudent);
 router.post("/login", loginStudent);
+router.get("/profile", protect, getProfile);
+router.post("/logout", logoutStudent);
 
 export default router;
