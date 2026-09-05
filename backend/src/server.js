401e1cd1a83de941af0d5324583dc23c9ff6d495
dilusharadeeshan <dilusharadeeshan@gmail.com>
2026-09-05 10:10:57 +0530

    feat: integrate CORS middleware for handling cross-origin requests

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,6 +6,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import authRoutes from "./routes/authRoutes.js";
 
@@ -15,6 +16,13 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true
+  })
+);
 
 //test route
 app.get('/', (req, res) => {
